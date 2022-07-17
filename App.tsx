386dafbb501539d26b62a64869d800ec95250816
Alysson Correia <arcorreiaa@gmail.com>
2022-07-17 14:55:18 -0300

    add api and get

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { ThemeProvider } from 'styled-components';
-import { Welcome } from './src/pages/Welcome';
+// import { Welcome } from './src/pages/Welcome';
 import theme from './src/global/styles/theme';
 import { StatusBar } from 'expo-status-bar';
+import { Home } from './src/pages/Home';
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
-        <Welcome />
+        {/* <Welcome /> */}
+        <Home />
     </ThemeProvider>
   );
 }
